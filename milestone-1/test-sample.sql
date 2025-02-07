5e67d0e4d8845d969f306f486c3161042f169833
Michael Zhang <yzhan666@gmail.com>
2025-02-07 01:23:54 -0500

    Added sample SQL query and output

---
+++ milestone-1/test-sample.sql
@@ -0,0 +1,12 @@
+-- Search movies with "Star Wars" in title, sorted by release year
+SELECT title, release_year, show_type
+FROM netflix_titles 
+WHERE LOWER(title) LIKE '%' || LOWER('star wars') || '%'
+ORDER BY release_year DESC;
+
+-- Search all titles in Thrillers genre
+SELECT title, release_year, show_type, listed_in
+FROM netflix_titles
+WHERE listed_in LIKE 'Thriller' || '%'
+ORDER BY release_year DESC
+LIMIT 5;
